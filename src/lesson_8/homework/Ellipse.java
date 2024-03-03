38c0aefcd4dce7af9a20ce9acd7b60f8550cbb6a
Guy Dzin <guydzin@gmail.com>
2024-03-03 17:01:18 +0300

    Обновляет домашнее задание расчетом эллипса

---
+++ src/lesson_8/homework/Ellipse.java
@@ -0,0 +1,15 @@
+package lesson_8.homework;
+
+public class Ellipse extends Circle {
+    double r2;
+    public Ellipse(double r,double r2, String name) {
+        super(r, name);
+        this.r2 = r2;
+    }
+
+    @Override
+    public void getPerimeter() {
+        super.perimeter = (4 * (Math.PI * r * r2) + (r - r2))/(r+r2);
+        System.out.printf("Фигура - %s. Её периметр равен - %.2f\n", name, perimeter);
+    }
+}
--- src/lesson_8/homework/Main.java
+++ src/lesson_8/homework/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Rectangle rectangle = new Rectangle(10,7,10,7,"Прямоугольник");
         Circle circle = new Circle(10,"Круг");
         Triangle triangle = new Triangle(10,15,22,"Треугольник");
+        Ellipse ellipse = new Ellipse(10,5,"Эллипс");
 
         rectangle.getPerimeter();
         circle.getPerimeter();
         triangle.getPerimeter();
+        ellipse.getPerimeter();
 
     }
 }
